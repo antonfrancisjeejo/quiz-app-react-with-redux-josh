1e6a791f6f82beb82ae7e1398f912f0b80dbae31
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-09-01 04:21:00 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as QuizHeader } from "./QuizHeader/QuizHeader";
+export { default as QuestionDisplay } from "./QuestionDisplay/QuestionDisplay";
+export { default as Option } from "./Option/Option";
+export { default as QuestionSelector } from "./QuestionSelector/QuestionSelector";

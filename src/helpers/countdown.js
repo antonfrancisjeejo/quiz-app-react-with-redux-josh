75f4f68147770d390f41784c8b6bc52c49097d9c
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-08-31 23:43:13 +0530

    Initial commit

---
+++ src/helpers/countdown.js
@@ -0,0 +1,18 @@
+function timer(duration) {
+  let seconds = duration % 60;
+  let secondsInMinutes = (duration - seconds) / 60;
+  let minutes = secondsInMinutes % 60;
+  let hours = (secondsInMinutes - minutes) / 60;
+
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  return {
+    hours,
+    minutes,
+    seconds,
+  };
+}
+
+export default timer;
